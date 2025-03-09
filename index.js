0ef4c7654a7c4e59d9ca63e3481c9b30f2d81717
Uservpr <danysaputra2682010@gmail.com>
2025-03-09 13:07:37 +0700

    add input in cli

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-// index.js
 const axios = require('axios');
+const readline = require('readline');
 
-// Inisialisasi baseUrl
-const baseUrl = 'http://localhost:3000/api/questions'; // Ganti dengan URL API yang sesuai
 
-// Fungsi GET
 const Get = async (url) => {
     try {
         const response = await axios.get(url);
@@ -47,9 +44,32 @@ const del = async (url) => {
         throw error;
     }
 };
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function askQuestion(query) {
+    return new Promise(resolve => rl.question(query, resolve));
+}
 
 async function main() {
-  const data = await Get(baseUrl)
+
+  // Minta method
+  const method = await askQuestion("Method [GET/POST/PUT/DEL]: ");
+  // Minta url
+  const url = await askQuestion("URL:  ");
+    
+    if (method === "GET") {
+      const data = await Get(URL)
+    } else if (method === "POST") {
+      const data = await post(URL)
+    } else if (method === "PUT") {
+      const data = await put(URL)
+    } else if (method === "DEL") {
+      const data = await del(URL)
+    }
   console.log(data)
+    rl.close();
 }
 main()
